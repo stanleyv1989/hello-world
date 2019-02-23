9d4bafeaced71facf9e41fd6ddbe005697bebf3a
Stanley V <stanislav.vysotskiy@pcc.edu>
2019-02-23 07:29:40 -0800

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by Stanislav Vysotskiy on 2/23/2019.
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
