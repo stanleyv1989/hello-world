79563cebb0b410b82fb129bc9cd5c8e43e35178e
Stanley V <stanislav.vysotskiy@pcc.edu>
2019-02-23 07:31:45 -0800

    added date message

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Stanislav Vysotskiy on 2/23/2019.
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
